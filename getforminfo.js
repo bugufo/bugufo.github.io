ad8590a36e0bfcd78270a31ef2899c0a74dd6e11
bugufo <80814337+bugufo@users.noreply.github.com>
2021-03-17 10:30:17 +0200

    Add files via upload

---
+++ getforminfo.js
@@ -0,0 +1,6 @@
+function getforminfo(){
+    var developer_user_both = document.getElementById("developer_or_user");
+    var developer_user_both_text = developer_user_both.value;
+	alert(developer_user_both_text);
+}
+
